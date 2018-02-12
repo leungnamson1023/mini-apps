8459e5b83fdf746d7b6ea3cdf9351011f41a8fe6
Namson Leung <leungnamson@gmail.com>
2018-02-12 12:11:18 -0800

    have a working grid that goes X first and then O, but that is all thus far

---
+++ challenge_1/client/app.js
@@ -0,0 +1,21 @@
+// need code so that when clicked on a specific cell/box, it will update it with either and X or and O
+
+// would need eventHandlers so that when clicked on a specific box, it will update it with an X or O
+
+// var ele1 = document.getElementById('oneByOne');
+// var ele2 = document.getElementById('oneByTwo');
+// var ele3 = document.getElementById('oneByThree');
+// var ele4 = document.getElementById('twoByOne');
+// var ele5 = document.getElementById('twoByTwo');
+// var ele6 = document.getElementById('twoByThree');
+// var ele7 = document.getElementById('threeByOne');
+// var ele8 = document.getElementById('threeByTwo');
+// var ele9 = document.getElementById('threeByThree');
+
+
+// var arr = [ele1, ele2, ele3, ele4, ele5, ele6, ele7, ele8, ele9];
+
+// for (var i = 0; i < 9; i++) {
+//   arr[i].addEventListener('click', () => {this.arr[i].innerHTML = 'X'});
+// };
+
