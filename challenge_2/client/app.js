8647eaffaf09a2f251156326d31ea64595d40322
Namson Leung <leungnamson@gmail.com>
2018-02-13 18:30:04 -0800

    added recursion

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -5,13 +5,32 @@ function post (e) {
   var dataType = document.getElementById('form').value;
   document.getElementById('form').value = '';
 
+  // var callback = function(data) {
+  //   var arr = [];
+  //   for (var key in data) {
+  //     arr.push(data[key]);
+  //   }
+  //   var list = arr.join(', ');
+  //   $('#list').append(`<li>${list}</li>`);
+  // }
+
   var callback = function(data) {
     var arr = [];
-    for (var key in data) {
-      arr.push(data[key]);
+
+    var recursion = function(person) {
+      if (!person.children.length) {
+        arr.push([person.firstName, person.lastName, person.county, person.city, person.role, person.sales]);
+      } else {
+        for (var i = 0; i < person.children.length; i++) {
+          arr.push([person.firstName, person.lastName, person.county, person.city, person.role, person.sales]);
+          recursion(person.children[i]);
+        }
+      }
     }
-    var list = arr.join(', ');
-    $('#list').append(`<li>${list}</li>`);
+    recursion(data);
+    arr.forEach(function(array, i) {
+      $('#list').append(`<li>${array[0]}, ${array[1]}, ${array[2]}, ${array[3]}, ${array[4]}, ${array[5]}</li>`)
+    });
   }
 
 
