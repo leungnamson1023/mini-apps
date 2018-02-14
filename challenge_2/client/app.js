0b76f74f3eae0d2edafcd1a41b9f1178b73f60d0
Namson Leung <leungnamson@gmail.com>
2018-02-13 18:06:22 -0800

    working csv if adding objects that are not nested with children

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,33 @@
+button.addEventListener('click', post);
+
+function post (e) {
+  e.preventDefault();
+  var dataType = document.getElementById('form').value;
+  document.getElementById('form').value = '';
+
+  var callback = function(data) {
+    var arr = [];
+    for (var key in data) {
+      arr.push(data[key]);
+    }
+    var list = arr.join(', ');
+    $('#list').append(`<li>${list}</li>`);
+  }
+
+
+  $.ajax({
+    url : '/',
+    type: 'POST',
+    data: dataType,
+    contentType: 'application/json',
+    dataType: 'json',
+    success: (data) => {
+      callback(data);
+    },
+    error: (err) => {
+      console.error(err);
+    }
+  })
+}
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,70 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
 
+
+
+
+
+// this handles static files, initial load of webpage
+app.use(express.static('./client'))
+
+// storage of the data of results, may not be needed *******
+var arrResults = [];
+// this handles the GET method with a route of csv
+// app.get('*', (req, res) => {
+//   fs.readFile('./samples/sales_report.json', (err, data) => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       arrResults.push(data.toString());
+//       arrResults.join('\n');
+//     }
+
+//     res.json(arrResults);
+
+//   });
+// });
+
+
+
+
+app.post('/', (req, res) => {
+  console.log(req.url, 'reqqqqq');
+  let body = [];
+  req.on('error', (err) => {
+    console.error(err);
+  }).on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    body = JSON.parse(body);
+    res.json(body);
+  })
+});
+
+
+
+// this handles the static file
+// app.use(express.static('./client'), function(req, res) {
+//   // this handles the GET route
+//   if (req.method === 'GET') {
+//    console.log('this is the get');
+//     var arr = [];
+//   fs.readFile('./samples/sales_report.json', 'utf-8', (err, data) => {
+//     arr.push(data);
+//   //  console.log(data[526]);
+//   })
+
+//  // this handles the POST route
+//   } else if (req.method === 'POST' && req.url === '/') {
+//     console.log('this is the post', req.info);
+//   } 
+// });
+
+
+// listening to server making sure it is up
+app.listen(3000, () => {
+  console.log('server is on Listening to 3000');
+});
 
